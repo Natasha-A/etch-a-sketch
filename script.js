c516eae195f3d6437e3d28db5868c63066a04948
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-05-26 15:55:41 -0400

    dynamic grid feature

---
+++ script.js
@@ -0,0 +1,54 @@
+
+/* Constants and variables */
+const container = document.getElementById("grid-container");
+
+/* Display value for range slider*/
+function updateTextInput(val) {
+  document.getElementById("textInput").value = val + ' x ' + val;
+
+  makeRows(val, val);
+}
+
+/* Default grid */
+function makeDefaultGrid() {
+  container.style.setProperty('--grid-rows', 16);
+  container.style.setProperty('--grid-cols', 16);
+
+  container.replaceChildren();
+  for (c = 0; c < (16 * 16); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item"
+  };
+};
+
+/* Add (rows, cols) divs to pixel grid*/
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+
+  container.replaceChildren();
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item"
+  };
+};
+
+/* Hover effect for grid divs to change cover when mouse passes over */
+
+const cells = container.children;
+
+console.log(cells);
+
+
+const cellArray = [...cells];
+
+console.log(cellArray);
+
+cellArray.forEach((cell)=> {
+  console.log(cell);
+});
+
+  // Load default grid on page
+//window.onload = makeDefaultGrid();
